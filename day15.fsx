28c89bf6f4519b5dcc567e8d4b64ea7a74d61b22
Martin Björkström <martin@bjorkstrom.dev>
2022-12-18 23:47:12 +0200

    Day 15 - part 2

--- day15.fsx
+++ day15.fsx
@@ -74,5 +74,57 @@ let part1 filename line =
     |> coverage line
     |> List.length
 
-part1 "test.txt" 10 |> printfn "Part 1: %d"
-part1 "day15.txt" 2000000 |> printfn "Part 1: %d"
+//part1 "test.txt" 10 |> printfn "Part 1: %d"
+//part1 "day15.txt" 2000000 |> printfn "Part 1: %d"
+
+// Part 2
+
+let outsidePoints sensor =
+    seq {
+        let max = sensor.Strength + 1
+        let (x,y) = (sensor.Position.X, sensor.Position.Y)
+        for i = max downto 1 do
+            let x1 = x + max - i
+            let x2 = x1 * (-1)
+            let x3 = x - i
+            let x4 = x3 * (-1)
+            let y1 = y - i
+            let y2 = y1 * (-1)
+            let y3 = y + max - i
+            let y4 = y3 * (-1)
+            yield (x1,y1)
+            yield (x2,y2)
+            yield (x3,y4)
+            yield (x4,y3)
+    }
+
+let findBeacon max sensors =
+    let mutable coordinate = (0,0)
+    sensors
+    |> Array.tryFind (fun sensor ->
+        sensor
+        |> outsidePoints
+        |> Seq.tryFind (fun (x,y) ->
+            if x < 0 || x > max || y < 0 || y > max then false
+            else
+                let found =
+                    sensors
+                    |> Array.forall (fun s ->
+                        let len = abs (x - s.Position.X) + abs (y - s.Position.Y)
+                        len > s.Strength)
+                if found then
+                    coordinate <- (x, y)
+                found)
+        |> Option.isSome)
+    |> ignore
+    coordinate
+
+
+let part2 filename max =
+    filename
+    |> parseSensorFile
+    |> findBeacon max
+    |> (fun (x,y) -> ((x |> uint64) * 4000000UL) + (y |> uint64))
+
+part2 "test.txt" 20// |> printfn "Part 2: %A"
+part2 "day15.txt" 4000000// |> printfn "Part 1: %d"
